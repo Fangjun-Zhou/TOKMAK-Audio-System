d130d37f94e1732fdc609654a1dabcdd470c7817
Fangjun Zhou <fzhou48@wisc.edu>
2021-08-07 05:08:03 +0800

    Added: AudioPlayer class Implement basic AudioPlayer functions

---
+++ Assets/Package/Runtime/AudioPlayer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace AudioSystem.Runtime
+{
+    public class AudioPlayer : MonoBehaviour, IAudioPlayer
+    {
+        #region IAudioPlayer
+
+        public void Play()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Resume()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
